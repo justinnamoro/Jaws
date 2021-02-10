d6a8a634f2186ea903e1e1c10ab0bab223063554
Your Name <you@example.com>
2021-02-09 19:40:30 -0800

    connection

--- mysqlDatabase.js
+++ mysqlDatabase.js
@@ -1,19 +1,12 @@
 const mysql = require('mysql')
 
 // 2
-if (process.env.JAWSDB_URL){
-  var connection = mysql.createConnection(process.env.JAWSDB_URL)
-} else {
-
-  const dbDetails = {
-    connectionLimit : 10,
-    host     : process.env.MYSQL_HOST || 'localhost',
-    user     : process.env.MYSQL_USERNAME || 'netflix_list_user',
-    password : process.env.MYSQL_PASSWORD || 'MyNewPass4!',
-    database : process.env.MYSQL_DATABASE || 'netflix_list'
-  }
-
-
+const dbDetails = {
+  connectionLimit : 10,
+  host     : process.env.MYSQL_HOST || 'l0ebsc9jituxzmts.cbetxkdyhwsb.us-east-1.rds.amazonaws.com',
+  user     : process.env.MYSQL_USERNAME || 'axw59h8flgu9l9d6',
+  password : process.env.MYSQL_PASSWORD || 'tahf8ed0rovkgzai',
+  database : process.env.MYSQL_DATABASE || 'c4loytn3n80o4y6a'
 }
 
   const connection = mysql.createConnection(dbDetails)
