a72088272504af63f542fa09ffd74f85c4320943
Your Name <you@example.com>
2021-02-09 20:18:02 -0800

    .

--- mysqlDatabase.js
+++ mysqlDatabase.js
@@ -1,26 +1,15 @@
 const mysql = require('mysql')
 
 // 2
-// const dbDetails = {
-//   connectionLimit : 10,
-//   host     : process.env.MYSQL_HOST || 'l0ebsc9jituxzmts.cbetxkdyhwsb.us-east-1.rds.amazonaws.com',
-//   user     : process.env.MYSQL_USERNAME || 'axw59h8flgu9l9d6',
-//   password : process.env.MYSQL_PASSWORD || 'tahf8ed0rovkgzai',
-//   database : process.env.MYSQL_DATABASE || 'c4loytn3n80o4y6a'
-// }
+const dbDetails = {
+  connectionLimit : 10,
+  host     : process.env.MYSQL_HOST || 'l0ebsc9jituxzmts.cbetxkdyhwsb.us-east-1.rds.amazonaws.com',
+  user     : process.env.MYSQL_USERNAME || 'axw59h8flgu9l9d6',
+  password : process.env.MYSQL_PASSWORD || 'tahf8ed0rovkgzai',
+  database : process.env.MYSQL_DATABASE || 'c4loytn3n80o4y6a'
+}
 
-//   const connection = mysql.createConnection(dbDetails)
-
-  if (process.env.JAWSDB_URL) {
-    var connection = mysql.createConnection(process.env.JAWSDB_URL)
-  } else {
-    var connection = mysql.createConnection({
-    host     : process.env.MYSQL_HOST || 'l0ebsc9jituxzmts.cbetxkdyhwsb.us-east-1.rds.amazonaws.com',
-    user     : process.env.MYSQL_USERNAME || 'axw59h8flgu9l9d6',
-    password : process.env.MYSQL_PASSWORD || 'tahf8ed0rovkgzai',
-    database : process.env.MYSQL_DATABASE || 'c4loytn3n80o4y6a'
-    })
-  }
+  const connection = mysql.createConnection(dbDetails)
   
   // 3
   function allMovies(callback) {
