8309c8758693399ac9571aa0fef774c1cf4d1c1e
Your Name <you@example.com>
2021-02-11 15:40:19 -0800

    .

--- server.js
+++ server.js
@@ -2,27 +2,27 @@ const express = require('express')
 const database = require('./mysqlDatabase.js');
 const app = express() 
 
-app.use('/', function(req, res, next) {
+// app.use('/', function(req, res, next) {
 
-  var origin = req.headers.origin;
+//   var origin = req.headers.origin;
 
-//   use this if you want to limit the domain allowed
-  var allowedOrigins = ['http://localhost:3000/', 'http://localhost:3306/'];
-  if(allowedOrigins.indexOf(origin) === -1){
-      next();
-    return;
-  }
-​
-   res.setHeader('Access-Control-Allow-Origin', origin || "");
-  // Request methods you wish to allow
-  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
-  // Request headers you wish to allow
-  res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
-  // Set to true if you need the website to include cookies in the requests sent
-  // to the API (e.g. in case you use sessions)
-  res.setHeader('Access-Control-Allow-Credentials', true);
-  next();
-});
+// //   use this if you want to limit the domain allowed
+//   var allowedOrigins = ['http://localhost:3000/', 'http://localhost:3306/'];
+//   if(allowedOrigins.indexOf(origin) === -1){
+//       next();
+//     return;
+//   }
+// ​
+//    res.setHeader('Access-Control-Allow-Origin', origin || "");
+//   // Request methods you wish to allow
+//   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+//   // Request headers you wish to allow
+//   res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+//   // Set to true if you need the website to include cookies in the requests sent
+//   // to the API (e.g. in case you use sessions)
+//   res.setHeader('Access-Control-Allow-Credentials', true);
+//   next();
+// });
 
 app.use(express.json())
 
